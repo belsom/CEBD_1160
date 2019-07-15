0a6379c8a9073669ff92c94de36540760c8252bb
belsom <49367305+belsom@users.noreply.github.com>
2019-07-14 21:40:30 -0400

    Update data_filename.sh

--- homework/class-2/data_filename.sh
+++ homework/class-2/data_filename.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
 echo "Please choose your extension for your file between these : csv, xlsx, pdf, doc, txt"
+
+do
+  read extension
+ case $extension in
+	[csv, xlsx, pdf, doc, txt])
+		touch selected.$extension
+    ls selected.$extension
+		;;
+	*)
+		echo "Sorry, choose between the given list"
+		;;
+  esac
+done
+echo "Thank you!"
