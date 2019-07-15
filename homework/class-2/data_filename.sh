b88a549cbbc096774b176f758f3ac00e04bd4269
belsom <49367305+belsom@users.noreply.github.com>
2019-07-14 22:26:47 -0400

    Update data_filename.sh

--- homework/class-2/data_filename.sh
+++ homework/class-2/data_filename.sh
@@ -4,7 +4,7 @@ echo "Please choose your extension for your file between these : csv, xlsx, pdf,
 
 read extension
  case $extension in
-	("csv" "xlsx" "pdf" "doc" "txt"))
+	("csv" "xlsx" "pdf" "doc" "txt")
 		echo "Creating the file selected.$extension"
 		touch selected.$extension
                 ls selected.$extension
